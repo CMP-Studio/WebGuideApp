b935ff67fb93fd03fdc50a70db1bc0546aa822d6
drew <mcdermotta@carnegiemuseums.org>
2016-04-28 16:36:50 -0400

    More tests

--- guide/update_data.py
+++ guide/update_data.py
@@ -1,10 +1,12 @@
+import requests
 import pprint
 #from guide.models import *
 
 sync_url = "http://guidecms.carnegiemuseums.org/api/v2/sync"
 
 # First we need to retrieve the data
-data = get_sync_data()
+r = requests.get(sync_url)
+data = r.json()
 #pretty print
 print "Here is the JSON:"
 pprint.pprint(data)
