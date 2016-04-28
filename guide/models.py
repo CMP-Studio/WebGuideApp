572007add7a4695a8a1535b7d8ecc1c5210334c9
drew <mcdermotta@carnegiemuseums.org>
2016-04-28 14:13:23 -0400

    Add artist and exhibition models

--- guide/models.py
+++ guide/models.py
@@ -3,3 +3,33 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    uuid            = models.UUIDField(primary_key=True)
+    created_at      = models.DateTimeField()
+    updated_at      = models.DateTimeField()
+    deleted_at      = models.DateTimeField()
+    first_name      = models.CharField(max_length=255)
+    last_name       = models.CharField(max_length=255)
+    country         = models.CharField(max_length=20)
+    bio             = models.TextField()
+    code            = models.CharField(max_length=40)
+    exhibition_uuid = models.ForeignKey('Exhibition', on_delete=models.CASCADE)
+
+class Exhibition(models.Model):
+    uuid                    = models.UUIDField(primary_key=True)
+    created_at              = models.DateTimeField()
+    updated_at              = models.DateTimeField()
+    deleted_at              = models.DateTimeField()
+    title                   = models.CharField(max_length=255)
+    subtitle                = models.CharField(max_length=255)
+    is_live                 = models.BooleanField()
+    position                = models.IntegerField()
+    sponsor                 = models.CharField(max_length=255)
+    bg_iphone_updated_at    = models.DateTimeField()
+    bg_ipad_updated_at      = models.DateTimeField()
+    bg_iphone_file_size     = models.IntegerField()
+    bg_ipad_file_size       = models.IntegerField()
+    bg_iphone_normal        = models.URLField()
+    bg_iphone_retina        = models.URLField()
+    bg_ipad_normal          = models.URLField()
+    bg_ipad_retina          = models.URLField()
