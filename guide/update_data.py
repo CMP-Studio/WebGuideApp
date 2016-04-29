fece144982e3684dbb9bf5fa73e525b1d8b2147b
drew <mcdermotta@carnegiemuseums.org>
2016-04-29 10:33:36 -0400

    Syntax errors fixed

--- guide/update_data.py
+++ guide/update_data.py
@@ -12,13 +12,14 @@ data = r.json()
 #check if data status is True
 if data['status']:
     print "Done!"
-else
+else:
     print "Data is not good.  Exiting."
     sys.exit(0)
 #Exhibitions
 print 'Processing Exhibitions...'
 exhib = data['exhibitions']
-Exhibition.object.all().delete()
+Exhibition.objects.all().delete()
 for key, e in exhib.items():
     exhib_obj = Exhibition(uuid=e.uuid, title=e.title)
     exhib_obj.save()
+print "Exhibitions are complete"
