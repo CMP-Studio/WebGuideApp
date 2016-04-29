2b7041477a162b70d00e6637b5c09d127aa6390a
drew <mcdermotta@carnegiemuseums.org>
2016-04-29 10:40:36 -0400

    Not sure what's wrong let's see if this works

--- guide/update_data.py
+++ guide/update_data.py
@@ -15,11 +15,14 @@ if data['status']:
 else:
     print "Data is not good.  Exiting."
     sys.exit(0)
+
 #Exhibitions
-print 'Processing Exhibitions...'
+print "Processing Exhibitions..."
 exhib = data['exhibitions']
 Exhibition.objects.all().delete()
 for key, e in exhib.items():
+    pprint.pprint(e)
     exhib_obj = Exhibition(uuid=e.uuid, title=e.title)
     exhib_obj.save()
+
 print "Exhibitions are complete"
