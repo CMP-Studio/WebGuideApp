2cbf575c94bc5032b54ce90bd42d2f74e1a493a4
drew <mcdermotta@carnegiemuseums.org>
2016-04-29 13:13:55 -0400

    Add 2 more classes for import

--- guide/update_data.py
+++ guide/update_data.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pprint
-from guide.models import Exhibition
+from guide.models import Location, Category, Exhibition
 
 #Functions
 def import_items(obj, data):
@@ -13,23 +13,42 @@ def import_items(obj, data):
 sync_url = "http://guidecms.carnegiemuseums.org/api/v2/sync"
 
 # First we need to retrieve the data
-print "Retrieving data from CMS..."
+print "\nRetrieving data from CMS..."
 r = requests.get(sync_url)
 data = r.json()
 #check if data status is True
 if data['status']:
-    print "Done!"
+    print "\nDone!"
 else:
-    print "Data is not good.  Exiting."
+    print "\nData is not good.  Exiting."
     sys.exit(0)
 
+#Location
+print "\nProcessing Locations..."
+entries = data['locations']
+Location.objects.all().delete()
+for e in entries:
+    obj = Location()
+    import_items(obj, e)
+    obj.save()
+print "\nLocations are complete"
+
+#Category
+print "\nProcessing Categories..."
+entries = data['categories']
+Category.objects.all().delete()
+for e in entries:
+    obj = Location()
+    import_items(obj, e)
+    obj.save()
+print "\nCategories are complete"
+
 #Exhibitions
-print "Processing Exhibitions..."
+print "\nProcessing Exhibitions..."
 exhib = data['exhibitions']
 Exhibition.objects.all().delete()
 for e in exhib:
     exhib_obj = Exhibition()
     import_items(exhib_obj, e)
     exhib_obj.save()
-
-print "Exhibitions are complete"
+print "\nExhibitions are complete"
