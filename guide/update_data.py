84412cd81aa98a9aa526470b9dc647cd0ab416d0
drew <mcdermotta@carnegiemuseums.org>
2016-04-29 10:12:43 -0400

    Working on data updating

--- guide/update_data.py
+++ guide/update_data.py
@@ -1,19 +1,15 @@
 import requests
+import json
 import pprint
 #from guide.models import *
 
 sync_url = "http://guidecms.carnegiemuseums.org/api/v2/sync"
 
 # First we need to retrieve the data
+print "Retrieving data from CMS..."
 r = requests.get(sync_url)
 data = r.json()
-#pretty print
-print "Here is the JSON:"
-pprint.pprint(data)
-print vars(data)
-
-def get_sync_data():
-    global sync_url
-    r = requests.get(sync_url)
-    data = r.json()
-    return data
+print "Done!"
+print data.status
+print "Processing exhibitions..."
+pprint.pprint(data.exhibitions)
