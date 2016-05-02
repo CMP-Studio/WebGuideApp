4e5af69b7dcbd1f1e8a3c6a9c5e46194da38fd14
drew <mcdermotta@carnegiemuseums.org>
2016-05-02 10:50:26 -0400

    Add url to force update

--- guide/views.py
+++ guide/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from guide.update_data import update_data
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+def update_data(request):
+    update_data()
+    return HttpResponse("Data has been updated!")
