d3b9c78ee91071e27bf441bcaf6908fcf8fcc105
drew <mcdermotta@carnegiemuseums.org>
2016-05-02 11:19:49 -0400

    Start templating

--- guide/admin.py
+++ guide/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Location, Category, Exhibition, Tour, Artist, Link, Artwork, Media, artistArtwork, tourArtwork
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Exhibition)
+admin.site.register(Tour)
+admin.site.register(Artist)
+admin.site.register(Link)
+admin.site.register(Artwork)
+admin.site.register(Media)
+admin.site.register(artistArtwork)
+admin.site.register(tourArtwork)
--- guide/views.py
+++ guide/views.py
@@ -5,7 +5,7 @@ from guide.update_data import update_from_CMS
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request, 'index.html')
 
 def update_data(request):
     update_from_CMS()
