1881a76fdef37ae09a87724db3b10bc4cc01fd11
drew <mcdermotta@carnegiemuseums.org>
2016-05-04 14:03:29 -0400

    Add videos tab

--- guide/urls.py
+++ guide/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^news$', views.news_show, name='news_show'),
+    url(r'^videos$', views.videos_show, name='videos_show'),
     url(r'^forceupdate$', views.update_data, name='force_update')
 ]
--- guide/views/news.py
+++ guide/views/news.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests, re
 
+#TODO: Move these to a settings field
 news_url = "http://guide-cms-prod.s3.amazonaws.com/feeds/news.html"
+videos_url = "http://guide-cms-prod.s3.amazonaws.com/feeds/videos.html"
 
 def news_show(request):
     r = requests.get(news_url)
@@ -10,5 +12,14 @@ def news_show(request):
     if s:
         context = {'news_text': s.group(0)}
     else:
-        content = {}
+        content = {'news_text':'<p>Sorry, we can\'t retrieve news at the moment, please try again later.</p>'}
     return render(request, 'news.html', context)
+
+def videos_show(request):
+    r = requests.get(videos_url)
+    s = re.search('(<style type="text/css">[\s\S]+</ul>)', r.text)
+    if s:
+        context = {'videos_text': s.group(0)}
+    else:
+        content = {'videos_text':'<p>Sorry, we can\'t retrieve videos at the moment, please try again later.</p>'}
+    return render(request, 'videos.html', context)
