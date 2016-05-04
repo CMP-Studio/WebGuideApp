aca3639a187ed13e4ae5f0b59c1753ef0a0b0279
drew <mcdermotta@carnegiemuseums.org>
2016-05-04 14:46:26 -0400

    rename to collections_list

--- guide/views/collections.py
+++ guide/views/collections.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from guide.models import Exhibition
 
-def exhibition_list(request):
+def collections_list(request):
     exhibs = Exhibition.objects.filter(is_live=True)
     context = {'collections': exhibs}
     return render(request, 'collections.html', context)
