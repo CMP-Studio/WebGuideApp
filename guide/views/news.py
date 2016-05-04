dede0b924cc4dbb1f897b9dcdc00aa82cf641d9c
drew <mcdermotta@carnegiemuseums.org>
2016-05-04 10:26:02 -0400

    Only get the juicy content of the news

--- guide/views/news.py
+++ guide/views/news.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
+import requests, re
 
 news_url = "http://guide-cms-prod.s3.amazonaws.com/feeds/news.html"
 
 def news_show(request):
     r = requests.get(news_url)
-    context = {'news_text': r.text}
+    s = re.search('(<style type="text/css">[\s\S]+)</body>', r.text)
+    if s:
+        context = {'news_text': s.group(0)}
+    else:
+        content = {}
     return render(request, 'news.html', context)
