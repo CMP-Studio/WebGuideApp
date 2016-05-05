2a3bfeabe224e8244ddd8073f8cf28c07f09dc7b
drew <mcdermotta@carnegiemuseums.org>
2016-05-05 11:25:28 -0400

    Import artwork

--- guide/views/objects.py
+++ guide/views/objects.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from guide.models import Exhibition
+from guide.models import Exhibition, Artwork
 
 def object_photos(request, slug):
     coll = Exhibition.objects.filter(slug=slug)
