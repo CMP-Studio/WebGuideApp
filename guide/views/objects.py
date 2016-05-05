cff4842795ae02a6f812863ed15e19fd67a3405e
drew <mcdermotta@carnegiemuseums.org>
2016-05-05 13:30:06 -0400

    add _set

--- guide/views/objects.py
+++ guide/views/objects.py
@@ -8,7 +8,7 @@ def object_photos(request, slug):
     coll = Exhibition.objects.filter(slug=slug)
     if coll:
         img = Media.objects.filter(kind='image', position='0')
-        art = Artwork.objects.filter(exhibition=coll).order_by('title').prefetch_related(Prefetch('media', queryset=img, to_attr='cover_img'))
+        art = Artwork.objects.filter(exhibition=coll).order_by('title').prefetch_related(Prefetch('media_set', queryset=img, to_attr='cover_img'))
         context = {'c': coll.first(), 'art': art}
 
         return render(request, "objects/photos.html" , context)
