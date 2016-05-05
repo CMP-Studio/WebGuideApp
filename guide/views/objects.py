48a6f0329628c599ef75d0276ff1807a99a7d65c
drew <mcdermotta@carnegiemuseums.org>
2016-05-05 11:27:16 -0400

    lowercase e

--- guide/views/objects.py
+++ guide/views/objects.py
@@ -6,7 +6,7 @@ from guide.models import Exhibition, Artwork
 def object_photos(request, slug):
     coll = Exhibition.objects.filter(slug=slug)
     if coll:
-        art = Artwork.objects.filter(Exhibition=coll)
+        art = Artwork.objects.filter(exhibition=coll)
         context = {'c': coll.first(), 'art': art}
 
         return render(request, "objects/photos.html" , context)
