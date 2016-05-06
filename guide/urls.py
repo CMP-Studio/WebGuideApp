ab55741fb60211ef05e6c95177f0d23fd02e50a5
drew <mcdermotta@carnegiemuseums.org>
2016-05-06 10:43:02 -0400

    Reworking urls

--- guide/urls.py
+++ guide/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     url(r'^videos\/*$', views.videos_show, name='videos_show'),
     url(r'^collections\/*$', views.collections_list, name='collections_list'),
     url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/*$', views.collection, name='collection'),
-    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/objects\/photos\/*$', views.object_photos, name='object_photos'),
-    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/objects\/list\/*$', views.object_list, name='object_list'),
-    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/objects\/category\/*$', views.object_cats, name='object_cats'),
-    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/objects\/category\/(?P<category>[a-z0-9\-]+)\/*$', views.object_category, name='object_category'),
-    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/objects\/(?P<object>[a-z0-9\-]+)\/*$', views.object, name='object'),
+    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/photos\/*$', views.object_photos, name='object_photos'),
+    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/list\/*$', views.object_list, name='object_list'),
+    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/category\/*$', views.object_cats, name='object_cats'),
+    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/category\/(?P<category>[a-z0-9\-]+)\/*$', views.object_category, name='object_category'),
+    url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/(?P<object>[a-z0-9\-]+)\/*$', views.object, name='object'),
     url(r'^collections\/(?P<collection>[a-z0-9\-]+)\/tours$', views.tours, name='tours'),
     url(r'^forceupdate$', views.update_data, name='force_update')
 ]
