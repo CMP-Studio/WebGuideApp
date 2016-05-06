b96a96945fcb132de86af7335e9fbb41e691ce0b
drew <mcdermotta@carnegiemuseums.org>
2016-05-06 14:35:05 -0400

    Start on tour page

--- guide/views/tours.py
+++ guide/views/tours.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from guide.models import Exhibition, Tour
+from guide.models import Exhibition, Tour, Artwork
 
 def tours(request, collection):
     coll_set = Exhibition.objects.filter(slug=collection, is_live=True)
@@ -15,4 +15,13 @@ def tours(request, collection):
 
 def tour(request, collection, tour):
     request.session['object-mode'] = 'tours/' + tour
-    return HttpResponse(tour)
+    coll_set = Exhibition.objects.filter(slug=collection, is_live=True)
+    tour_set = Tour.objects.filter(slug=tour)
+    if coll_set and tour_set:
+        coll = coll_set.first()
+        tour = tour_set.first()
+        art = Artwork.objects.filter(exhibition=coll, tour=tour).order_by('tourartwork__position')
+        context = {'c': coll, 'tour': tour, 'art': art}
+        return render(request, "tours/tour.html" , context)
+    else:
+        return HttpResponse("Not Found")
